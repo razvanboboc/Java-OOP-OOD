26cd0fa1f306d3fb476acf5e882039c81367cb74
RazvanMariusBoboc <rzvn11@gmail.com>
2020-02-21 05:15:46 +0200

    -Deleted old solutions

--- Week1/Week1P2Boboc/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main 
-{
-	
-    public static void main(String[] args) {
-    	
-    	PrimeNumbers primeNumbers = new PrimeNumbers();
-    	primeNumbers.start(Integer.parseInt(args[0]));
-    }
-}
