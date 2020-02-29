84de254841416c13cc57128d95ac1c74d62f7b01
RazvanMariusBoboc <rzvn11@gmail.com>
2020-02-29 18:23:23 +0200

    Formatted two lines for better visibility.

--- Week3/Week3P1RazvanBoboc/src/main/java/com/iquestgroup/remotelearning/App.java
+++ Week3/Week3P1RazvanBoboc/src/main/java/com/iquestgroup/remotelearning/App.java
@@ -2,14 +2,16 @@ package com.iquestgroup.remotelearning;
 
 public class App {
     public static void main(String[] args) {
-        Person englishPerson = new EnglishPerson("Will", "Smith","1968/25/09");
-        Person germanPerson = new GermanPerson("Hans Der Erste Jurgen II","1980/11/05");
+        Person englishPerson = new EnglishPerson("Will", "Smith", "1968/25/09");
+        Person germanPerson = new GermanPerson("Hans Der Erste Jurgen II", "1980/11/05");
 
         englishPerson.selfDescribe();
         germanPerson.selfDescribe();
 
-        System.out.println("The birthdate of " + englishPerson.getFirstName() +  " " + englishPerson.getSurName() + " in English Format is " + englishPerson.getBirthDate());
-        System.out.println("The birthdate of " + germanPerson.getFirstName() +  " " + germanPerson.getSurName() + " in German Format is " + germanPerson.getBirthDate());
+        System.out.println("The birthdate of " + englishPerson.getFirstName() + " " + englishPerson.getSurName() +
+                " in English format is " + englishPerson.getBirthDate());
+        System.out.println("The birthdate of " + germanPerson.getFirstName() + " " + germanPerson.getSurName() +
+                " in German format is " + germanPerson.getBirthDate());
     }
 }
 
